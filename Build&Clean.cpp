29cfd5dd1d17080058af6d88550cb2ee4c5e77be
neon_arch <mustafadhuleb53@gmail.com>
2023-03-11 12:34:24 +0300

    making pkgbuilds

--- Build&Clean.cpp
+++ Build&Clean.cpp
@@ -1,24 +1,33 @@
 // #include <iostream>
-#include <filesystem>
 #include <cstdlib>
+#include <filesystem>
 #include <string>
 
-int main(int argc, char *argv[])
-{
-  std::system("rm -rf ../neon-os-core-repo/x86_64/*");
-  for (const auto & entry : std::filesystem::directory_iterator(std::string("x86_64")))
-    {
-      std::system(("cd " + std::string(entry.path()) + "&& makepkg -cf --sign").c_str());
-      for (const auto & pkg : std::filesystem::directory_iterator(std::string(entry.path())))
-	  {
-	    if(std::string(pkg.path()).substr(std::string(pkg.path()).length()-3,3) == "zst" || std::string(pkg.path()).substr(std::string(pkg.path()).length()-4,3) == "zst" || std::string(pkg.path()).substr(std::string(pkg.path()).length()-3,3) == "sig" || std::string(pkg.path()).substr(std::string(pkg.path()).length()-4,3) == "sig" )
-	      {
-		std::filesystem::rename(std::string(pkg.path()),"../neon-os-core-repo/x86_64/"+std::string(pkg.path()).substr(std::string(entry.path()).length()+1));
-	      }
-	    if (std::filesystem::is_directory(std::string(pkg.path()))) {
-	      std::filesystem::remove_all(std::string(pkg.path()));
-	    }
-	  }
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  system("rm -rf ../sos-core/x86_64/*");
+  for (const auto &entry : filesystem::directory_iterator(string("x86_64"))) {
+    system(("cd " + string(entry.path()) + "&& makepkg -cf --sign").c_str());
+    for (const auto &pkg :
+         filesystem::directory_iterator(string(entry.path()))) {
+      if (string(pkg.path()).substr(string(pkg.path()).length() - 3, 3) ==
+              "zst" ||
+          string(pkg.path()).substr(string(pkg.path()).length() - 4, 3) ==
+              "zst" ||
+          string(pkg.path()).substr(string(pkg.path()).length() - 3, 3) ==
+              "sig" ||
+          string(pkg.path()).substr(string(pkg.path()).length() - 4, 3) ==
+              "sig") {
+        filesystem::rename(
+            string(pkg.path()),
+            "../sos-core/x86_64/" +
+                string(pkg.path()).substr(string(entry.path()).length() + 1));
+      }
+      if (filesystem::is_directory(string(pkg.path()))) {
+        filesystem::remove_all(string(pkg.path()));
+      }
     }
+  }
   return 0;
 }
